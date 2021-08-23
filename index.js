74c479047c09b7b7d28fd2a7cdec9342a5916021
coddy0717 <86177840+coddy0717@users.noreply.github.com>
2021-08-22 22:38:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+//Tarea de Construccion//
+const express = require('express') ;
+
+const app = express()
+
+app.use(express.json())
+
+let notes=[ 
+    {
+        'id': 1,
+        'content': "Adios",
+        'date': "2021-08-18T10:36:31.098Z",
+        'important': true
+    },
+    {
+        'id': 2,
+        'content': "Tengo que aprender boostrap",
+        'date': "2021-08-18T10:36:31.098Z",
+        'important': false
+    },
+    {
+        'id': 3,
+        'content': "Profundizar y repasar HTML",
+        'date': "2021-08-18T10:36:31.098Z",
+        'important': true
+    }
+]
+/* const app=http.createServer((request, response)=>{ 'request is declared but is value is never request parameter'
+    response.writeHead(200,{'Content-Type':'application/json'})
+    response.end(JSON.stringify(notes))
+}) */
+
+app.get('/',(request,response) => {
+    response.send('<h1>Hola Mundo</h1>')
+})
+
+app.get('/api/notes',(request,response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/id',(request,response) => {
+    const id=Number(request.params.id)
+    console.log({id})
+    const note= notes.find(note=> note.id === id)
+    console.log({note})
+    if (note){
+        response.json(notes)
+    }else{
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request,response)=>{
+    const id=Number(request.params.id)
+    notes=notes.filter(note=>note.id!=id)
+    response.status(204).end()
+})
+app.post('/api/notes',(request,response)=>{
+    const note =request.body
+
+    if(!note || note.content){
+        return response.status(400).json({
+            error: 'note content is missing'
+        })
+    }
+    console.log(note)
+    const ids=notes.map(notes=> note.id)
+    const maxId =Math.max(...ids)
+
+    const newNote={        
+        id: maxId+1,
+        content: note.content,
+        important: typeof note.important== 'undefined'? note.important:false,
+        date:new Date().toISOString()         
+    }  
+    notes=[...notes, newNote]
+    response.json(newNote)
+})
+const PORT=3001
+app.listen(PORT,()=>{
+    console.log('Server running on port ${PORT}')
+})
